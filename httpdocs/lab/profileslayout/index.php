f7bc743446c729514ebbb946e5fa0d3d3fe45ae9
jstewa61 <jstewa61@kent.edu>
2018-02-27 17:10:33 -0500

    Added Html Patterns

---
+++ httpdocs/lab/profileslayout/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Change Me</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <!--Included Header -->
+    <?php include '../patterns/header.php';?>
+
+
+    <!--Use this code whenever you need to include a shared component -->
+    <?php include '../patterns/sample.php';?>
+
+
+
+    <!--Included Footer -->
+    <?php include '../patterns/footer.php';?>
+  </body>
+</html>
