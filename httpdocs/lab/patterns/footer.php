4578ec3e17a372fb575c1881fc6dd59f09edadf6
Nicole C <ncramer3@kent.edu>
2018-03-02 12:04:34 -0500

    Footer HTML

--- httpdocs/lab/patterns/footer.php
+++ httpdocs/lab/patterns/footer.php
@@ -1,5 +1,30 @@
 <footer>
-  This is the footer
+  <div>
+    <h4> Contact </h4>
+    <h5> Phone: (330)678-2712</h5>
+    <h5> Kent Historical Society </br> 237 E. Main St. </br> Kent OH 44240</h5>
+  </div>
+  <div>
+    <h4> Hours</h4>
+    <h5> Friday & Saturday </br> 9 A.M. - 2 P.M.</h5>
+    <h5> ADMISSION IS ALWAYS FREE</h5>
+  </div>
+  <div>
+    <div> KHS LOGO HERE </div>
+    <ul>
+      <li> <a href= "#"> Join </a></li>
+      <li> <a href= "#"> Give </a></li>
+      <li> <a href="#"> Learn</a></li>
+    </ul>
+    <ul>
+      <li> <a href="#"> Visit </a></li>
+      <li> <a href="#"> Support</a></li>
+      <li> <a href="#"> Resources </a></li>
+      <li> Search Bar </li>
+      <li> <a href="#"> About Us </a></li>
+      <li> <a href="#"> Shop </a></li>
+    </ul>
+  </div>
 </footer>
 
 
