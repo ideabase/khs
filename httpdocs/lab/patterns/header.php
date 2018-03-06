6e3d23645c745d8ecd7545a3408494d49037f3d9
jstewa61 <jstewa61@kent.edu>
2018-03-06 16:31:34 -0500

    HTML for header

--- httpdocs/lab/patterns/header.php
+++ httpdocs/lab/patterns/header.php
@@ -10,6 +10,25 @@
 <!-- Don't touch above this -->
 
 
-    <header>
-      This is the header
-    </header>
+<header>
+  <img src="" alt="KHS Logo">
+  <nav role="navigation">
+    <input type="search" placeholder="Can't find something?">
+    <div>
+    <ul>
+        <li><a href="#">Join</a></li>
+        <li><a href="#">Give</a><li>
+        <li><a href="#">Learn</a></li>
+    </ul>
+  </div>
+  <div>
+    <ul>
+        <li><a href="#">Visit</a></li>
+        <li><a href="#">Support</a><li>
+        <li><a href="#">Resources</a></li>
+        <li><a href="#">About Us</a><li>
+        <li><a href="#">Shop</a></li>
+    </ul>
+  </div>
+  </nav>
+</header>
