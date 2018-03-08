1448a76c73dfeda82547c08fc7aa9f979c9f6926
jstewa61 <jstewa61@kent.edu>
2018-03-08 13:19:01 -0500

    Added HTML structure for home

--- httpdocs/lab/home/index.php
+++ httpdocs/lab/home/index.php
@@ -1,4 +1,12 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Change Me</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
     <!--Included Header -->
     <?php include '../patterns/header.php';?>
 
@@ -6,7 +14,21 @@
     <!--Use this code whenever you need to include a shared component -->
     <?php include '../patterns/sample.php';?>
 
+    <div>
+      <img src="" alt="alt text"/>
+      <h1>This is the headline</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div>
+      <h2>This is headline 2</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/uP_0DkpFOKs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+
 
 
     <!--Included Footer -->
     <?php include '../patterns/footer.php';?>
+  </body>
+</html>
