4390ec38e20256adfacf7337bb0af978b24656cf
Nicole C <ncramer3@kent.edu>
2018-03-11 13:47:29 -0400

    Final Edits to Secondary Layout

--- httpdocs/lab/secondarypagelayout/index.php
+++ httpdocs/lab/secondarypagelayout/index.php
@@ -12,40 +12,40 @@
 
 
     <!--Use this code whenever you need to include a shared component -->
+<body>
 <h1>This is Headline 1</h1>
-
-    <?php include '../patterns/para.php';?>
-
 <h2>This Is Headline 2</h2>
-    <body>
-      <div>
-        <h1> Headline</h1>
-        <h2> Subhead</h2>
-        <p> This is a paragraph. This paragraph would be an overview of what is on the page. </p>
-        <button type="button"> This is a button </button>
-      <figure>
-        <img src="#" alt="This is a feature image">
-      </figure>
-      </div>
+<figure>
+  <img src="#" alt="This is a feature image">
+</figure>
+<h1> Headline</h1>
+<h2> Subhead</h2>
+  <?php include '../patterns/para.php';?>
+  <button type="button"> This is a button </button>
     <img src="#" alt="this is a background image">
-    <div>
       <h1> Headline</h1>
     <div>
       <div>
         <img src="#">
+        <span>
         <h3> Header 3 </h3>
+      </span>
         <p> This is a paragraph. This will describe the link below and image. </p>
         <button type="button"> This is a button </button>
       </div>
         <div>
         <img src="#">
+        <span>
         <h3> Header 3 </h3>
+      </span>
         <p> This is a paragraph. This will describe the link below and image. </p>
         <button type="button"> This is a button </button>
       </div>
         <div>
         <img src="#">
+        <span>
         <h3> Header 3 </h3>
+      </span>
         <p> This is a paragraph. This will describe the link below and image. </p>
         <button type="button"> This is a button </button>
       </div>
