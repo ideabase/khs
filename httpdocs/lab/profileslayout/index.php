5901b60606147ea02d5f44f764599112d9c8afa6
Nicole C <ncramer3@kent.edu>
2018-03-11 13:40:59 -0400

    Content Heavy Layout Complete
    accidentally posted content heavy HTML in the Profiles Layout. Has been corrected.

--- httpdocs/lab/profileslayout/index.php
+++ httpdocs/lab/profileslayout/index.php
@@ -8,35 +8,6 @@
   </head>
   <body>
 
-<h1>This is Headline 1</h1>
-
-<h2>This Is Headline 2</h2>
-<figure>
-  <img src="#">
-</figure>
-<figcaption> This is an image caption</figure>
-<p> This is a paragraph on the content heavy page. Tempor dolor magna irure eiusmod excepteur veniam, esse magna veniam, cupidatat dolore ut ex in ad eiusmod officia mollit nulla lorem ipsum sit in ut duis lorem adipisicing est amet, eiusmod et cupidatat non qui nisi tempor commodo irure voluptate exercitation pariatur. Tempor sed labore ipsum culpa fugiat enim occaecat aliqua. Exercitation eu ipsum ipsum aute laboris elit, cillum i.</p>
-<?php include '../patterns/quote.php';?>
-<p> This is a paragraph on the content heavy page. Tempor dolor magna irure eiusmod excepteur veniam, esse magna veniam, cupidatat dolore ut ex in ad eiusmod officia mollit nulla lorem ipsum sit in ut duis lorem adipisicing est amet, eiusmod et cupidatat non qui nisi tempor commodo irure voluptate exercitation pariatur. Tempor sed labore ipsum culpa fugiat enim occaecat aliqua. Exercitation eu ipsum ipsum aute laboris elit, cillum i.</p>
-<h3> This is Header 3 </h3>
-<p> This is a paragraph on the content heavy page. Tempor dolor magna irure eiusmod excepteur veniam, esse magna veniam, cupidatat dolore ut ex in ad eiusmod officia mollit nulla lorem ipsum sit in ut duis lorem adipisicing est amet, eiusmod et cupidatat non qui nisi tempor commodo irure voluptate exercitation pariatur. Tempor sed labore ipsum culpa fugiat enim occaecat aliqua. Exercitation eu ipsum ipsum aute laboris elit, cillum i.</p>
-<?php include '../patterns/quote.php';?>
-<figure>
-  <img src="#">
-</figure>
-<figcaption> This is an image caption</figure>
-  <h4>List of Similar articles </h4>
-   <ul>
-     <li> <a href="#"> Article 1</li>
-     <li> <a href="#"> Article 2</li>
-     <li> <a href="#"> Article 3</li>
-  </ul>
-<h4>List of Similar articles </h4>
-   <ul>
-     <li> <a href="#"> Article 1</li>
-     <li> <a href="#"> Article 2</li>
-     <li> <a href="#"> Article 3</li>
-  </ul> 
     <!--Included Footer -->
     <?php include '../patterns/footer.php';?>
   </body>
