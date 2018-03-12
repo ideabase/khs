81d1fddadfce274581d843486b514c15593fa7be
Nicole C <ncramer3@kent.edu>
2018-03-12 11:42:12 -0400

    corrected email html

--- httpdocs/lab/patterns/profile1.php
+++ httpdocs/lab/patterns/profile1.php
@@ -3,5 +3,5 @@
 </figure>
 <h3> First and Last Name</h3>
 <h4> Phone Number </h4>
-<h4> <a href="mailto:example@email.com"> Email </h4>
+<h4> <a href="mailto:example@email.com"> Email Me</a></h4>
   <p> This is a paragraph. orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
