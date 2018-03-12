9f7cfda331f5c440fa65eb6604685bfd03db1507
Nicole C <ncramer3@kent.edu>
2018-03-12 11:39:42 -0400

    profile, and 2 patterns for profile made

--- httpdocs/lab/profileslayout/index.php
+++ httpdocs/lab/profileslayout/index.php
@@ -7,7 +7,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
+    <header>
+    <?php include '../patterns/header.php';?>
+    <h1>This is Headline 1</h1>
+    <h2>This Is Headline 2</h2>
+  </header>
+    <h1> Headline</h1>
+    <h2> Subhead</h2>
+      <?php include '../patterns/para.php';?>
 
+  <h3> Headline</h3>
+  <?php include '../patterns/profile1.php';?>
+  <?php include '../patterns/profile1.php';?>
+  <?php include '../patterns/profile1.php';?>
+
+  <h3> Headline</h3>
+  <?php include '../patterns/profile2.php';?>
+  <?php include '../patterns/profile2.php';?>
+  <?php include '../patterns/profile2.php';?>
     <!--Included Footer -->
     <?php include '../patterns/footer.php';?>
   </body>
