90efac3676f3d5e0d55a5a315298917a712a6e6b
Nicole C <ncramer3@kent.edu>
2018-03-23 13:22:02 -0400

    Feature w/Link HTML created, Profiles2 edited, and LGpara created

---
+++ httpdocs/lab/FeaturewLink/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Change Me</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <?php include '../patterns/header.php';?>
+  <header>
+    <figure>
+      <img src="#" alt="this is the banner image">
+    </figure>
+    <h1> This is a Headline</h1>
+    <h2> This is a Subhead</h2>
+  </header>
+  <?php include '../patterns/LGpara.php';?>
+  <h3> Headline </h3>
+  <h4> Subhead</h4>
+  <img src="#" alt="this is an image">
+  <?php include '../patterns/para.php';?>
+  <button type="button"> This is a button </button>
+  <?php include '../patterns/profile2.php';?>
+  <?php include '../patterns/profile2.php';?>
+</body>
+  <footer>
+  <?php include '../patterns/footer.php';?>
+</footer>
+</html>
---
+++ httpdocs/lab/patterns/LGpara.php
@@ -0,0 +1,1 @@
+<h5> This is a large/call out paragraph. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</h5>
