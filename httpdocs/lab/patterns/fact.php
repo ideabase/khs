6babbb6ecd148fc2c073f24856f1351cb59f2877
Nicole C <ncramer3@kent.edu>
2018-04-09 13:17:39 -0400

    Timeline HTML

---
+++ httpdocs/lab/patterns/fact.php
@@ -0,0 +1,4 @@
+<div>
+<h3> Date/Year </h3>
+<p> Sentence or two about what happened this year. Lorem ipsum dolor sit amet, consectetur adipisicing elit. <p>
+</div> 
--- httpdocs/lab/timeline/index.php
+++ httpdocs/lab/timeline/index.php
@@ -1,12 +1,49 @@
-
-    <!--Included Header -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Page Title</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
     <?php include '../patterns/header.php';?>
+    <div>
+      <img src="../images/bannerimage.jpg" alt="alt text"/>
+      <h1>This is the Page headline</h1>
+    </div>
+<main>
+      <h3>This is an article headline</h3>
+      <?php include '../patterns/para.php';?>
+      <?php include '../patterns/para.php';?>
+      <?php include '../patterns/quote.php';?>
+      <img src="#" alt="This is an image">
 
+<div>
+  <h2> This is a Section Headline</h2>
+      <?php include '../patterns/profile2.php';?>
+      <?php include '../patterns/profile2.php';?>
+      <?php include '../patterns/profile2.php';?>
+</div>
 
-    <!--Use this code whenever you need to include a shared component -->
-    <?php include '../patterns/sample.php';?>
-
-
-
+<div>
+  <div>
+  <h2> This is a Section Headline </h2>
+  <?php include '../patterns/para.php';?>
+  <div>
+    <?php include '../patterns/fact.php';?>
+    <?php include '../patterns/fact.php';?>
+    <img src="#" alt="This is an image">
+    <img src="#" alt="This is an image">
+    <?php include '../patterns/fact.php';?>
+    <?php include '../patterns/fact.php';?>
+    <img src="#" alt="This is an image">
+    <?php include '../patterns/fact.php';?>
+    <?php include '../patterns/fact.php';?>
+    <img src="#" alt="This is an image">
+    <?php include '../patterns/fact.php';?>
+  </div>
+</div>
+</main>
     <!--Included Footer -->
     <?php include '../patterns/footer.php';?>
