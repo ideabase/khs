523fc13702e6d2c883b228723642d6dc1ce41c2b
Jacob <jlisic@kent.edu>
2018-04-10 10:30:40 -0400

    Edited HTML and CSS for Header and Footer

--- httpdocs/lab/patterns/footer.php
+++ httpdocs/lab/patterns/footer.php
@@ -1,30 +1,30 @@
 <footer>
   <div>
-    <h4> Contact </h4>
-    <h5> Phone: (330)678-2712</h5>
-    <h5> Kent Historical Society </br> 237 E. Main St. </br> Kent OH 44240</h5>
+    <h4>Contact</h4>
+    <h5>Phone: (330) 678-2712</h5>
+    <h5>Kent Historical Society </br>237 E. Main St. </br>Kent OH 44240</h5>
   </div>
   <div>
-    <h4> Hours</h4>
-    <h5> Friday & Saturday </br> 9 A.M. - 2 P.M.</h5>
-    <h5> ADMISSION IS ALWAYS FREE</h5>
+    <h4>Hours</h4>
+    <h5>Friday & Saturday </br>9 A.M. - 2 P.M.</h5>
+    <h5>ADMISSION IS ALWAYS FREE</h5>
   </div>
   <div>
-    <div class="smalllogo"> KHS LOGO HERE</div>
+    <div class="smalllogo">KHS LOGO HERE</div>
     <ul class="callout-footer">
-      <li> <a href= "#"> Join </a></li>
-      <li> <a  href= "#"> Give </a></li>
-      <li> <a  href="#"> Learn</a></li>
+      <li> <a href= "#">Join</a></li>
+      <li> <a  href= "#">Give</a></li>
+      <li> <a  href="#">Learn</a></li>
     </ul>
   </div>
     <div class="menu-options">
     <ul>
-      <li> <a class="menu-options" href="#"> Visit </a></li>
-      <li> <a class="menu-options" href="#"> Support</a></li>
-      <li> <a class="menu-options" href="#"> Resources </a></li>
-      <li> Search Bar </li>
-      <li> <a class="menu-options" href="#"> About Us </a></li>
-      <li> <a class="menu-options" href="#"> Shop </a></li>
+      <li class="main-nav-links"><a class="menu-options" href="#">Visit</a></li>
+      <li class="main-nav-links"><a class="menu-options" href="#">Support</a></li>
+      <li class="main-nav-links"><a class="menu-options" href="#">Resources</a></li>
+      <input type="search" placeholder="Search" class="main-nav-links">
+      <li class="main-nav-links"><a class="menu-options" href="#">About Us</a></li>
+      <li class="main-nav-links"><a class="menu-options" href="#">Shop</a></li>
     </ul>
   </div>
 </footer>
--- httpdocs/lab/patterns/header.php
+++ httpdocs/lab/patterns/header.php
@@ -6,69 +6,85 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
-  <body>
+<body>
 <!-- Don't touch above this -->
 
 
-<header>
-  <div class="header-top">
-  <img src="" alt="KHS Logo">
-  <nav role="navigation">
-    <div>
-    <ul class="callout-header">
-        <li><a href="#">Join</a></li>
-        <li><a href="#">Give</a><li>
-        <li><a href="#">Learn</a></li>
-    </ul>
-    <input type="search" placeholder="Search">
-  </div>
-</div>
+  <header>
+    <div class="header-top">
+      <img src="#" alt="KHS Logo">
+      <div class="callout-nav-container">
+        <nav role="navigation">
+          <div>
+            <ul class="callout-header">
+                <li>
+                  <a href="#">Join</a>
+                </li>
+                <li>
+                  <a href="#">Give</a>
+                  <li>
+                <li>
+                  <a href="#">Learn</a>
+                </li>
+            </ul>
+            <input type="search" placeholder="Search">
+          </div>
+        </nav>
+      </div>
+    </div>
+      <div class="main-nav-container">
+        <nav role="navigation">
+          <!-- Mobile Menu Button -->
+            <div class="nav-mobile"><a id="nav-toggle" href="#"><span></span></a></div>
+          <!-- End Mobile Menu Button -->
+        <div>
+          <ul class="nav-main">
+          <li class="nav-links">
+            <a href="#">Visit</a>
+              <ul class="nav-dropdown">
+                <li><a href="#">Location & Hours</a></li>
+                <li><a href="#">Exhibits</a></li>
+                <li><a href="#">Events & Calandar</a></li>
+              </ul>
+          </li>
 
-  <div>
-    <ul class="nav-main">
-    <li>
-      <h2><a href="#">Visit</a></h2>
-        <ul>
-          <li><a href="#">Location & Hours</a></li>
-          <li><a href="#">Exhibits</a></li>
-          <li><a href="#">Events & Calandar</a></li>
-        </ul>
-    </li>
+          <li class="nav-links">
+            <a href="#">Support</a>
+              <ul class="nav-dropdown">
+                <li><a href="#">Membership</a></li>
+                <li><a href="#">Donate</a></li>
+                <li><a href="#">Volunteer</a></li>
+              </ul>
+          </li>
 
-    <li>
-      <h2><a href="#">Support</a></h2>
-        <ul>
-          <li><a href="#">Membership</a></li>
-          <li><a href="#">Donate</a></li>
-          <li><a href="#">Volunteer</a></li>
-        </ul>
-    </li>
+          <li class="nav-links">
+            <a href="#">Collections & Resources</a>
+              <ul class="nav-dropdown">
+                <li><a href="#">Kent Tribune</a></li>
+                <li><a href="#">Historic Landmarks</a></li>
+                <li><a href="#">City History</a></li>
+                <li><a href="#">Residential History</a></li>
+                <li><a href="#">Photo & Video Archive</a></li>
+                <li><a href="#">Artifacts</a></li>
+              </ul>
+          </li>
 
-    <li>
-      <h2><a href="#">Collections & Resources</a></h2>
-        <ul>
-          <li><a href="#">Kent Tribune</a></li>
-          <li><a href="#">Historic Landmarks</a></li>
-          <li><a href="#">City History</a></li>
-          <li><a href="#">Residential History</a></li>
-          <li><a href="#">Photo & Video Archive</a></li>
-          <li><a href="#">Artifacts</a></li>
-        </ul>
-    </li>
+          <li class="nav-links">
+            <a href="#">About Us</a>
+              <ul class="nav-dropdown">
+                <li><a href="#">About the KHS</a></li>
+                <li><a href="#">Meet the Team</a></li>
+                <li><a href="#">Our Homes</a></li>
+              </ul>
+          </li>
 
-    <li>
-      <h2><a href="#">About Us</a></h2>
-        <ul>
-          <li><a href="#">About the KHS</a></li>
-          <li><a href="#">Meet the Team</a></li>
-          <li><a href="#">Our Home</a></li>
-        </ul>
-    </li>
-
-    <li>
-      <h2><a href="#">Shop</a></h2>
-    </li>
-    <ul>
-  </div>
-  </nav>
-</header>
+          <li class="nav-links">
+            <a href="#">Shop</a>
+          </li>
+          <ul>
+        </div>
+      </div>
+      </nav>
+  </header>
+  <script src="scripts.js"></script>
+</body>
