658014dab53c96f9d78eb14c486a1df3950b3025
Ciara <clillis@kent.edu>
2018-04-10 11:51:28 -0400

    Edits to home page

--- httpdocs/lab/patterns/profile1.php
+++ httpdocs/lab/patterns/profile1.php
@@ -1,7 +1,20 @@
-<figure>
+<!--<figure>
   <img src="../images/ProfilePhoto.jpg">
 </figure>
 <h3> First and Last Name</h3>
 <h4> Phone Number </h4>
 <h4> <a href="mailto:example@email.com"> Email Me</a></h4>
   <p> This is a paragraph. orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+-->
+  <figure class="holder">
+
+    <figcaption class="title">
+      <h3> First and Last Name</h3>
+      <h4> Phone Number </h4>
+      <h4> <a href="mailto:example@email.com"> Email Me</a></h4>
+        <p> This is a paragraph. orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </figcaption>
+
+    <img src="../images/ProfilePhoto.jpg"/>
+
+   </figure>
