5bb52c8de693e481b3b534c021921de43db9292a
Jacob <jlisic@kent.edu>
2018-04-24 17:29:45 -0400

    Edited homepage and Our Homes page

--- httpdocs/lab/timeline/index.php
+++ httpdocs/lab/timeline/index.php
@@ -13,37 +13,37 @@
       <h1>This is the Page headline</h1>
     </div>
 <main>
-      <h3>This is an article headline</h3>
-      <?php include '../patterns/para.php';?>
-      <?php include '../patterns/para.php';?>
-      <?php include '../patterns/quote.php';?>
-      <img src="#" alt="This is an image">
+        <h3>This is an article headline</h3>
+        <?php include '../patterns/para.php';?>
+        <?php include '../patterns/para.php';?>
+        <?php include '../patterns/quote.php';?>
+        <img src="#" alt="This is an image">
 
-<div>
-  <h2> This is a Section Headline</h2>
-      <?php include '../patterns/profile2.php';?>
-      <?php include '../patterns/profile2.php';?>
-      <?php include '../patterns/profile2.php';?>
-</div>
-
-<div>
+  <!-- side profile crap -->
   <div>
-  <h2> This is a Section Headline </h2>
-  <?php include '../patterns/para.php';?>
-  <div>
-    <?php include '../patterns/fact.php';?>
-    <?php include '../patterns/fact.php';?>
-    <img src="#" alt="This is an image">
-    <img src="#" alt="This is an image">
-    <?php include '../patterns/fact.php';?>
-    <?php include '../patterns/fact.php';?>
-    <img src="#" alt="This is an image">
-    <?php include '../patterns/fact.php';?>
-    <?php include '../patterns/fact.php';?>
-    <img src="#" alt="This is an image">
-    <?php include '../patterns/fact.php';?>
+    <h2> This is a Section Headline</h2>
+        <?php include '../patterns/profile2.php';?>
+        <?php include '../patterns/profile2.php';?>
+        <?php include '../patterns/profile2.php';?>
   </div>
-</div>
+
+<!-- This is the code for the timeline grid graphic looking thing -->
+  <div>
+    <h2> This is a Section Headline </h2>
+    <?php include '../patterns/para.php';?>
+    <div>
+      <?php include '../patterns/fact.php';?>
+      <?php include '../patterns/fact.php';?>
+      <img src="#" alt="This is an image">
+      <img src="#" alt="This is an image">
+      <?php include '../patterns/fact.php';?>
+      <?php include '../patterns/fact.php';?>
+      <img src="#" alt="This is an image">
+      <?php include '../patterns/fact.php';?>
+      <?php include '../patterns/fact.php';?>
+      <img src="#" alt="This is an image">
+      <?php include '../patterns/fact.php';?>
+  </div>
 </main>
     <!--Included Footer -->
     <?php include '../patterns/footer.php';?>
