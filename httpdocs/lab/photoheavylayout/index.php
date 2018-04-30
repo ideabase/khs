9db019ddfc9478776ff0a9239a1baaa5ad9f7e78
Amrita <adatta@kent.edu>
2018-04-30 11:20:36 -0400

    Had to recolve merge conflicts

--- httpdocs/lab/photoheavylayout/index.php
+++ httpdocs/lab/photoheavylayout/index.php
@@ -24,6 +24,9 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
