b0158671dab3a46fface2e618e9caf29863c2c8f
npappas27 <npappas@ideabasekent.com>
2018-05-05 11:19:07 -0400

    Added 404 page

---
+++ httpdocs/lab/404 Page/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Change Me</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+
+    <!--Included Header -->
+    <?php include '../patterns/header.php';?>
+
+    <?php include '../patterns/banner.php';?>
+    <?php include '../patterns/khshours.php';?>
+
+    <body>
+      <main>
+      <?php include '../patterns/intropara.php';?>
+
+   </main>
+
+     <!--Included Footer -->
+     <?php include '../patterns/footer.php';?>
+   </body>
+ </html>
