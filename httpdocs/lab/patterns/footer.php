adfa915c268c97eb1325e1c67fa9bc6a6cf85511
Ciara <clillis@kent.edu>
2018-05-07 16:55:28 -0400

    Added some things into the footer
    Still needs styled

--- httpdocs/lab/patterns/footer.php
+++ httpdocs/lab/patterns/footer.php
@@ -1,17 +1,21 @@
 <footer>
-  <div class="footer-container">
-  <div>
+<div class="footer-container">
+    <a href="../home" class="footer-logo"><img src="../images/KHS_stacked_white.svg" alt="KHS Logo"></a>
+    <div class="footer-contact-hours">
+  <div class="footer-contact">
     <h4>Contact</h4>
     <h5>Phone: (330) 678-2712</h5>
     <h5>Kent Historical Society </br>237 E. Main St. </br>Kent OH 44240</h5>
   </div>
-  <div>
+  <div class="footer-hours">
     <h4>Hours</h4>
     <h5>Friday & Saturday </br>9 A.M. - 2 P.M.</h5>
     <h5>ADMISSION IS ALWAYS FREE</h5>
   </div>
-  <div>
-    Social Media
+</div>
+  <div class="footer-social">
+    <a href="../home" class="facebook"><img src="../images/facebook-green.svg" alt="facebook"></a>
+    <a href="../home" class="instagram"><img src="../images/instagram-green.svg" alt="instagram"></a>
   </div>
   <div class="menu-options">
       <input type="search" placeholder="Search" class="main-nav-links">
