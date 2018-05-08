b9b9d186af1802bee745765ced1b5b2ee0d87c29
npappas27 <npappas@ideabasekent.com>
2018-05-08 18:13:38 -0400

    Added styling to shop page

--- httpdocs/lab/patterns/shopitem.php
+++ httpdocs/lab/patterns/shopitem.php
@@ -1,5 +1,5 @@
 <div class="item">
-<img src="payloom-23-detail_1.png">
+<img src="../images/master_1537.jpg">
 <h3> Item Title </h3>
 <p> This is a description of the item. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 <button type="button"> Add To Cart </button>
