947108e567d355965db1efb6c947b7a7dfff674b
npappas27 <npappas@ideabasekent.com>
2018-05-08 18:00:00 -0400

    Worked on footer and profiles page

--- httpdocs/lab/patterns/footer.php
+++ httpdocs/lab/patterns/footer.php
@@ -1,25 +1,46 @@
 <footer>
 <div class="footer-container">
-    <a href="../home" class="footer-logo"><img src="../images/KHS_stacked_white.svg" alt="KHS Logo"></a>
-    <div class="footer-contact-hours">
-  <div class="footer-contact">
-    <h4>Contact</h4>
-    <h5>Phone: (330) 678-2712</h5>
-    <h5>Kent Historical Society </br>237 E. Main St. </br>Kent OH 44240</h5>
+  <div class="footer-logo-cta-container">
+    <div class="footer-logo-container">
+      <a href="../home" class="footer-logo"><img src="../images/KHS_stacked_white.svg" alt="KHS Logo"></a>
+    </div>
+    <div class="callout-footer">
+      <ul>
+        <a href="/"> <li>Join</li></a>
+        <a href="/"> <li> Give</li></a>
+        <a href="/"> <li> Learn</li></a>
+      </ul>
+    </div>
   </div>
-  <div class="footer-hours">
-    <h4>Hours</h4>
-    <h5>Friday & Saturday </br>9 A.M. - 2 P.M.</h5>
-    <h5>ADMISSION IS ALWAYS FREE</h5>
+
+  <div class="footer-contact-hours">
+    <div class="footer-contact">
+      <h4>Contact</h4>
+      <h5>Phone: (330) 678-2712</h5>
+      <h5>Kent Historical Society </br>237 E. Main St. </br>Kent OH 44240</h5>
+    </div>
+    <div class="footer-hours">
+      <h4>Hours</h4>
+      <h5>Friday & Saturday </br>9 A.M. - 2 P.M.</h5>
+      <h5>ADMISSION IS ALWAYS FREE</h5>
+    </div>
   </div>
-</div>
-  <div class="footer-social">
-    <a href="../home" class="facebook"><img src="../images/facebook-green.svg" alt="facebook"></a>
-    <a href="../home" class="instagram"><img src="../images/instagram-green.svg" alt="instagram"></a>
+  <div class="social-media__icons">
+    <a class="social-media__icon Facebook" href="{{ block.siteURL }}">Facebook</a>
+    <a class="social-media__icon Instagram" href="{{ block.siteURL }}">Instagram</a>
   </div>
   <div class="menu-options">
-      <input type="search" placeholder="Search" class="main-nav-links">
+    <ul>
+      <a href="/"> <li>Visit</li></a>
+      <a href="/"> <li>Support</li></a>
+      <a href="/"> <li>Resources</li></a>
+      <a href="/"> <li>About Us</li></a>
+      <a href="/"> <li>Shop</li></a>
+    </ul>
   </div>
+</div>
+<div class="footer-copyright">
+  <p> Copyight 2018 All rights Reserved </p>
 </div>
 </footer>
 
