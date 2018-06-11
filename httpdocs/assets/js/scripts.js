82c5f3f0d801bdff46572e73b76716196183e32c
Christopher Hallahan <challahan@gmail.com>
2018-06-11 16:47:50 -0400

    Fixed paths to favicons, Google Analytics

--- httpdocs/assets/js/scripts.js
+++ httpdocs/assets/js/scripts.js
@@ -1,50 +1,63 @@
 // JavaScript Goes Here //
 
-//Script//
+// Place all Jquery dependencies here.  Won't run until Jquery is loaded //
 
-// Script for Dropdown Menu
-(function($) { // Begin jQuery
-  $(function() { // DOM ready
-    // If a link has a dropdown, add sub menu toggle.
-    $('nav .nav-main li a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
-      // Close one dropdown when selecting another
-      $('.nav-dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
-    });
-    // Clicking away from dropdown will remove the dropdown class
-    $('html').click(function() {
-      $('.nav-dropdown').hide();
-    });
-    // Toggle open and close nav styles on click
-    $('#nav-toggle').click(function() {
-      $('nav .nav-main').slideToggle();
-    });
-    // Hamburger to X toggle
-    $('#nav-toggle').on('click', function() {
-      this.classList.toggle('active');
-    });
-  }); // end DOM ready
-})(jQuery); // end jQuery
-// End Script for Dropdown Menu
+(function() {
+  var nTimer = setInterval(function() {
+    if (window.jQuery) {
 
-// Card Accordion //
+      // Begin Jquery code //
 
-$(document).ready(function(){
-    $('.card-container').click(function(){
-      $(this).toggleClass('active');
-      $('.card-container').not($(this)).removeClass('active');
-    });
-});
+      // Script for Dropdown Menu
+      (function($) { // Begin jQuery
+        $(function() { // DOM ready
+          // If a link has a dropdown, add sub menu toggle.
+          $('nav .nav-main li a:not(:only-child)').click(function(e) {
+            $(this).siblings('.nav-dropdown').toggle();
+            // Close one dropdown when selecting another
+            $('.nav-dropdown').not($(this).siblings()).hide();
+            e.stopPropagation();
+          });
+          // Clicking away from dropdown will remove the dropdown class
+          $('html').click(function() {
+            $('.nav-dropdown').hide();
+          });
+          // Toggle open and close nav styles on click
+          $('#nav-toggle').click(function() {
+            $('nav .nav-main').slideToggle();
+          });
+          // Hamburger to X toggle
+          $('#nav-toggle').on('click', function() {
+            this.classList.toggle('active');
+          });
+        }); // end DOM ready
+      })(jQuery); // end jQuery
+      // End Script for Dropdown Menu
 
-$(".banner-h1").html(function(){
-  var text= $(this).text().trim().split(" ");
-  var first = text.shift();
-  return (text.length > 0 ? "<span class='first'>"+ first + "</span> " : first) + text.join(" ");
-});
+      // Card Accordion //
 
-$("main h2").html(function(){
-  var text= $(this).text().trim().split(" ");
-  var first = text.shift();
-  return (text.length > 0 ? "<span class='first'>"+ first + "</span> " : first) + text.join(" ");
-});
+      $(document).ready(function(){
+          $('.card-container').click(function(){
+            $(this).toggleClass('active');
+            $('.card-container').not($(this)).removeClass('active');
+          });
+      });
+
+      $(".banner-h1").html(function(){
+        var text= $(this).text().trim().split(" ");
+        var first = text.shift();
+        return (text.length > 0 ? "<span class='first'>"+ first + "</span> " : first) + text.join(" ");
+      });
+
+      $("main h2").html(function(){
+        var text= $(this).text().trim().split(" ");
+        var first = text.shift();
+        return (text.length > 0 ? "<span class='first'>"+ first + "</span> " : first) + text.join(" ");
+      });
+
+      // End Jquery code //
+
+      clearInterval(nTimer);
+    }
+  }, 100);
+})();
